641b4da8fc15e62c52f2774400cdc18f93490fac
fonyodigusztav <fonyodi.guszto@gmail.com>
2023-10-27 19:15:12 +0200

    Created region for original rooms

--- WorldOfZuul/Game.cs
+++ WorldOfZuul/Game.cs
@@ -15,24 +15,29 @@
 
         private void CreateRooms()
         {
-  
-            Room? outside = new("Outside", "You are standing outside the main entrance of the university. To the east is a large building, to the south is a computing lab, and to the west is the campus pub.");
-            Room? theatre = new("Theatre", "You find yourself inside a large lecture theatre. Rows of seats ascend up to the back, and there's a podium at the front. It's quite dark and quiet.");
-            Room? pub = new("Pub", "You've entered the campus pub. It's a cozy place, with a few students chatting over drinks. There's a bar near you and some pool tables at the far end.");
-            Room? lab = new("Lab", "You're in a computing lab. Desks with computers line the walls, and there's an office to the east. The hum of machines fills the room.");
-            Room? office = new("Office", "You've entered what seems to be an administration office. There's a large desk with a computer on it, and some bookshelves lining one wall.");
 
-            outside.SetExits(null, theatre, lab, pub); // North, East, South, West
+            #region OriginalRooms
+            //Room? outside = new("Outside", "You are standing outside the main entrance of the university. To the east is a large building, to the south is a computing lab, and to the west is the campus pub.");
+            //Room? theatre = new("Theatre", "You find yourself inside a large lecture theatre. Rows of seats ascend up to the back, and there's a podium at the front. It's quite dark and quiet.");
+            //Room? pub = new("Pub", "You've entered the campus pub. It's a cozy place, with a few students chatting over drinks. There's a bar near you and some pool tables at the far end.");
+            //Room? lab = new("Lab", "You're in a computing lab. Desks with computers line the walls, and there's an office to the east. The hum of machines fills the room.");
+            //Room? office = new("Office", "You've entered what seems to be an administration office. There's a large desk with a computer on it, and some bookshelves lining one wall.");
 
-            theatre.SetExit("west", outside);
+            //outside.SetExits(null, theatre, lab, pub); // North, East, South, West
 
-            pub.SetExit("east", outside);
+            //theatre.SetExit("west", outside);
 
-            lab.SetExits(outside, office, null, null);
+            //pub.SetExit("east", outside);
 
-            office.SetExit("west", lab);
+            //lab.SetExits(outside, office, null, null);
 
-            currentRoom = outside;
+            //office.SetExit("west", lab);
+
+            //currentRoom = outside;
+            #endregion
+            Room? outside = new("Outside", "You are standing outside of a recycling centre");
+            
+
         }
 
         public void Play()
