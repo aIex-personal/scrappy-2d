013614c5cb555290310c691f9d4e98edfba930e8
fonyodigusztav <fonyodi.guszto@gmail.com>
2023-10-29 12:37:31 +0100

    player class created

---
+++ WorldOfZuul/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldOfZuul
+{
+    public class Player
+    {
+        public int health { get; set; }
+        public Player(int health)
+        {
+                this.health = health;
+        }
+    }
+}
