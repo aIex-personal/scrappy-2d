e00363b9696a07293172ae56f376b8a50fae992b
fonyodigusztav <fonyodi.guszto@gmail.com>
2023-11-06 15:38:08 +0100

    merged rooms descriptions edit

--- WorldOfZuul/Game.cs
+++ WorldOfZuul/Game.cs
@@ -22,18 +22,47 @@ namespace WorldOfZuul
         {
             //Creating the rooms, in the constructor there is the name of the room, and after that there is a 
             //description with the possible doors to open. 
-            Room? outside = new("Outside", "You are standing outside of a recycling centre. To the north, there seems to be an entrance to the building.");
-            Room? hall = new("Hall", "You find yourself in the hall. To the south is your exit from the building, and there are signs above the other doors. To the West, there seems to be a sorting room, to the North it says E-waste recycling, and to the East, the sign says Paper mill.");
-            Room? sortingRoom = new("Sorting Room", "It seems that people in this room are separating garbages. They have different places to collect Paper, Plastic, Organic Junk and Metal. To the North, there is a door that says Upcycling Studio, and to the East there is the Hall");
-            Room? plasticRecycling = new("Plastic Recycling", "In this room people seem to know a lot about the recycling of plastic. To the North, there is a room called Paper Mill, and to the West there is the hall.");
-            Room? upcyclingStudio = new("Upcycling Studio", "In this room people are creating new things out of old, battle-worn things. The door to the West seems to be an exit to a Composting Garden, the sign on the door to the North says Ocean Cleanup, to the East there is Recycled Art Gallery, and to the South there is a Sorting Room.");
-            Room? compostingGarden = new("Composting Garden", "In this garden there are loads of used Organical stuff, like the skins of vegetables, the remains of fruits etc. The Garden is surrounded by hedge, so the only entrance is to the East, back to the Upcycling Studio.");
-            Room? eWasteRecycling = new("E-Waste Recycling", "In this Room people are repairing old electrical stuff. To the North, there is a Recycled art gallery, to the East there is a Paper Mill, to the South there is the Hall and to the West there is an Upcycling Studio");
-            Room? paperMill = new("Paper Mill","In this room people know a lot about recycling paper. To the North there is a room related to planting trees, to the South there is a room about Plastic Recycling and to the West there is a room about recycling e-waste.");
-            Room? oceanCleanup = new("Ocean Cleanup", "People are thinking about solutions for cleaning the ocean from garbage. To the East there is a Recycled art Gallery and to the South there is an Upcycling Studio");
-            Room? recycledArtGallery = new("Recycled Art Gallery", "This room seems to be a museum about great actions of recyclement from all around the world. To the North, there is a locked door, to the East there is a room about Planting trees, to the South there is a room about E-waste and to the East there is a room related to cleaning the Oceans ");
-            Room? plantingTrees = new("Planting Trees", "People here are looking for the best way to plant more and more trees all around the world. To the South there is a Paper Mill and to the West there is a Recycled Art Gallery.");
-            Room? mysteryRoom = new("Final Mission Room", "WOW");
+            Room? outside = new("Outside", 
+                "You are standing outside of a recycling centre. To the north, there seems to be an entrance to the building.",
+                "First Time at outside");
+            Room? hall = new("Hall", 
+                @"
+You find yourself in the hall. To the south is your exit from the building, 
+and there are signs above the other doors. To the West, there seems to be a sorting room,
+to the North it says E-waste recycling, and to the East, the sign says Paper mill.", 
+                "First time at the Hall");
+            Room? sortingRoom = new("Sorting Room", 
+                @"
+                It seems that people in this room are separating garbages. 
+They have different places to collect Paper, Plastic, 
+Organic Junk and Metal. To the North, there is a door that says Upcycling Studio, 
+and to the East there is the Hall",
+                "First time at the sorting Room");
+            Room? plasticRecycling = new("Plastic Recycling", 
+                @"
+In this room people seem to know a lot about the recycling of plastic. 
+To the North, there is a room called Paper Mill, and to the West there is the hall.",
+                "First time at the PlasticRecyclingRoom");
+            Room? upcyclingStudio = new("Upcycling Studio", 
+                @"
+In this room people are creating new things out of old, 
+battle-worn things. The door to the West seems to be an exit to a Composting Garden, 
+the sign on the door to the North says Ocean Cleanup, to the East there is Recycled Art Gallery, 
+and to the South there is a Sorting Room.",
+                "First time at the UpcyclingStudio");
+            Room? compostingGarden = new("Composting Garden", "In this garden there are loads of used Organical stuff, like the skins of vegetables, the remains of fruits etc. The Garden is surrounded by hedge, so the only entrance is to the East, back to the Upcycling Studio.",
+                "First time at the CompostingGarden");
+            Room? eWasteRecycling = new("E-Waste Recycling", "In this Room people are repairing old electrical stuff. To the North, there is a Recycled art gallery, to the East there is a Paper Mill, to the South there is the Hall and to the West there is an Upcycling Studio",
+                "First time at the eWasteRecycling");
+            Room? paperMill = new("Paper Mill","In this room people know a lot about recycling paper. To the North there is a room related to planting trees, to the South there is a room about Plastic Recycling and to the West there is a room about recycling e-waste.",
+                "First time at the paperMill");
+            Room? oceanCleanup = new("Ocean Cleanup", "People are thinking about solutions for cleaning the ocean from garbage. To the East there is a Recycled art Gallery and to the South there is an Upcycling Studio",
+                "First time at the oceanCleanupRoom");
+            Room? recycledArtGallery = new("Recycled Art Gallery", "This room seems to be a museum about great actions of recyclement from all around the world. To the North, there is a locked door, to the East there is a room about Planting trees, to the South there is a room about E-waste and to the East there is a room related to cleaning the Oceans ",
+                "First time at the recycledArtGallery");
+            Room? plantingTrees = new("Planting Trees", "People here are looking for the best way to plant more and more trees all around the world. To the South there is a Paper Mill and to the West there is a Recycled Art Gallery.",
+                "First time at the plantingTreesRoom");
+            Room? mysteryRoom = new("Final Mission Room", "WOW", "First time at the mysteryRoom");
 
             //north, east, south, west
             //Setting the relations between rooms. 
