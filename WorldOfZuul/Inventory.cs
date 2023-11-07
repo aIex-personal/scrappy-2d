1560da8dfae649377c015375035141ee572a297b
fonyodigusztav <fonyodi.guszto@gmail.com>
2023-11-07 11:47:27 +0100

    created Inventory class

---
+++ WorldOfZuul/Inventory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    public class Inventory
+    {
+        private List<string> items;
+        public Inventory()
+        {
+            items = new List<string>();
+        }
+        //public List<string> ReadAll()
+        //{
+        //    return items;
+        //}
+        public void ReadAllConsole()
+        {
+            foreach (var item in items) { Console.WriteLine("item"); }
+        }
+        public void Add(string item)
+        {
+            items.Add(item);
+        }
+        public void Drop(string item)
+        {
+            if (items.Contains(item))
+            {
+                items.Remove(item);
+            }
+        }
+    }
+}
